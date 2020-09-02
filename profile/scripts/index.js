e7c681652d7c7be96650b70f7d955e2603ddb639
thinnaphats <68946650+thinnaphats@users.noreply.github.com>
2020-09-02 23:03:46 +0700

    2020-2-9 <add index.js>

--- profile/scripts/index.js
+++ profile/scripts/index.js
@@ -1,6 +1,5 @@
 let para = document.querySelector('button');
-var userEmail = document.getElementsByName('email').values;
 para.addEventListener('click', confirmEmail);
 function confirmEmail(){
-    var name = confirm("Your email address: " + userEmail);
+    var name = alert("Done");
 }
