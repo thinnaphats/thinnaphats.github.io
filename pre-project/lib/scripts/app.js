206e0cb0827ca0fe789eec6975ce9fe7629ed62d
thinnaphats <thinnaphats@kkumail.com>
2020-10-20 11:04:21 +0700

    test project

---
+++ pre-project/lib/scripts/app.js
@@ -0,0 +1,47 @@
+/* Range input to select the components */
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function () {
+    output.innerHTML = this.value;
+}
+var slider2 = document.getElementById("myRange2");
+var output2 = document.getElementById("demo2");
+output2.innerHTML = slider2.value;
+
+slider2.oninput = function () {
+    output2.innerHTML = this.value;
+}
+var slider3 = document.getElementById("myRange3");
+var output3 = document.getElementById("demo3");
+output3.innerHTML = slider3.value;
+
+slider3.oninput = function () {
+    output3.innerHTML = this.value;
+}
+var slider4 = document.getElementById("myRange4");
+var output4 = document.getElementById("demo4");
+output4.innerHTML = slider4.value;
+
+slider4.oninput = function () {
+    output4.innerHTML = this.value;
+}
+
+/* Button to simulate */
+
+var simButton = document.getElementById("simButton");
+simButton.onclick = function () {
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("result_wing_w");
+    output.innerHTML = slider.value;
+    var slider2 = document.getElementById("myRange2");
+    var output2 = document.getElementById("result_wing_l");
+    output2.innerHTML = slider2.value;
+    var slider3 = document.getElementById("myRange3");
+    var output3 = document.getElementById("result_body_w");
+    output3.innerHTML = slider3.value;
+    var slider4 = document.getElementById("myRange4");
+    var output4 = document.getElementById("result_body_l");
+    output4.innerHTML = slider4.value;
+}
