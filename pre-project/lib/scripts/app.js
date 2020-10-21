ff5ebbb2e0a221d63e752a7b9074d34369265206
thinnaphats <thinnaphats@kkumail.com>
2020-10-21 10:32:13 +0700

    update prj 2020-10-21 <thinnaphats>

--- pre-project/lib/scripts/app.js
+++ pre-project/lib/scripts/app.js
@@ -29,8 +29,10 @@ slider4.oninput = function () {
 }
 
 /* Button to simulate */
-
 var simButton = document.getElementById("simButton");
+const add = document.querySelector('img');
+
+/*get valued */
 simButton.onclick = function () {
     var slider = document.getElementById("myRange");
     var output = document.getElementById("result_wing_w");
@@ -44,4 +46,45 @@ simButton.onclick = function () {
     var slider4 = document.getElementById("myRange4");
     var output4 = document.getElementById("result_body_l");
     output4.innerHTML = slider4.value;
+
+        /*
+        *Helicopter Drop
+        */
+        
+        const scaleKeyframes = [
+          { transform: 'translateY(0px)'},
+        { transform: 'translateY(500px)'}
+        ]
+        
+        const scaleTiming = {
+          duration: 1500,
+          direction: 'alternate',
+          fill: 'none',
+          easing: 'ease-in-out'
+        }
+        
+
+        const rotateKeyframes = [
+          {transform: 'rotateY(0deg)'},
+          {transform: 'rotateY(360deg)'}
+          
+        ]
+        const rotateTiming = {
+          duration: 150,
+          iterations: 10,
+          direction: 'alternate',
+          easing: 'ease-in'
+        }
+        
+       
+       
+        
+         add.animate(scaleKeyframes, Object.assign({composite: 'add'}, scaleTiming));
+        let rotate = add.animate(rotateKeyframes, Object.assign({composite: 'add'}, rotateTiming)); 
+       
+      
+        
+     
 }
+
+
