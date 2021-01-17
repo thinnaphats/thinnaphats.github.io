f66555be9a80b1072653f01ff8fadbb8512e3100
thinnaphats <thinnaphats@kkumail.com>
2021-01-17 22:43:53 +0700

    clear repository 2021/1/17 <thinnaphats>

--- 6130404900-EN813701-2020-final/problem4/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const router = require('./students');
-var app = express();
-
-var student = require('./students.json')
-
-app.use('/students', student);
-
-app.listen(4000);
